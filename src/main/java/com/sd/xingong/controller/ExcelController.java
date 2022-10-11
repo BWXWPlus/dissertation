ae4801d8cef862b5ef477681f17522e32ba8a2fe
wuwenshuo <1412167111@qq.com>
2022-10-11 10:09:32 +0800

    所有功能基本实现，10/11

---
+++ src/main/java/com/sd/xingong/controller/ExcelController.java
@@ -0,0 +1,126 @@
+package com.sd.xingong.controller;
+
+import com.sd.xingong.mapper.TeacherMapper;
+import com.sd.xingong.pojo.Student;
+import com.sd.xingong.pojo.Teacher;
+import com.sd.xingong.service.StudentService;
+import com.sd.xingong.service.TeacherService;
+import com.sd.xingong.util.ExcelUtils;
+import com.sd.xingong.vo.StudentCount;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@CrossOrigin
+public class ExcelController {
+
+
+    Map<Integer,String> map2 = new HashMap();
+
+    @Autowired
+    private StudentService studentService;
+    @Autowired
+    private TeacherService teacherService;
+    @GetMapping("/excel/getAll")
+    public String getStudents(@RequestParam("teacherName") String teacherName , @RequestParam("stuClass") String stuClass){
+
+
+        StudentCount studentCount = studentService.searchStudents("", "", "", stuClass, teacherName, 1, 100000);
+        List<Student> students = studentCount.getStudent();
+        List<Map<String, Object>> mapArrayList = new ArrayList<>();
+         int j =1;
+        for (Student student : students) {
+
+            Map<String, Object> map = new HashMap<>();
+
+            map.put("学生姓名", student.getName());
+            map.put("学号", student.getStudentId());
+            if( student.getSpecialities() != null){
+                map.put("专业", student.getSpecialities());
+            }else {
+                map.put("专业", "无");
+            }
+
+            map.put("班级", student.getStuClass());
+            if(student.getTitle() != null){
+                map.put("设计（论文）题目", student.getTitle());
+            }else {
+                map.put("设计（论文）题目", "无");
+            }
+            if(student.getThesisType() != null){
+                map.put("成果类别(论文/设计)", student.getThesisType());
+            }else {
+                map.put("成果类别(论文/设计)", "无");
+            }
+            if(student.getNatureTopicSelection() ==0){
+                map.put("选题性质(开题报告写法一致)", "无");
+            }else if(student.getNatureTopicSelection() == 1){
+                map.put("选题性质(开题报告写法一致)", "理论研究");
+            }else if(student.getNatureTopicSelection() == 2){
+                map.put("选题性质(开题报告写法一致)", "应用研究");
+            }else if(student.getNatureTopicSelection() == 3){
+                map.put("选题性质(开题报告写法一致)", "技术开发");
+            }else if(student.getNatureTopicSelection() == 4){
+                map.put("选题性质(开题报告写法一致)", "工程设计");
+            }else {
+                map.put("选题性质(开题报告写法一致)", "无");
+            }
+            if(student.getSourceTopicSelection() == 0){
+                map.put("选题来源(开题报告写法一致)", "无");
+            }else  if(student.getSourceTopicSelection() == 1){
+                map.put("选题来源(开题报告写法一致)", "指定选题");
+            }else  if(student.getSourceTopicSelection() == 2){
+                map.put("选题来源(开题报告写法一致)", "自拟选题");
+            }else {
+                map.put("选题来源(开题报告写法一致)", "无");
+            }
+
+            map.put("序号", j);
+            j++;
+            Teacher teacher = teacherService.getATeacherById((int) student.getMentorId());
+            Teacher teacher2 = teacherService.getATeacherById((int) student.getMentor2Id());
+            if(teacher != null){
+                map.put("指导教师(姓名)", teacher.getName());
+                if(teacher.getTeacherTitle() != null){
+                    map.put("指导教师(职称)", teacher.getTeacherTitle());
+                }else {
+                    map.put("指导教师(职称)", "无");
+                }
+
+            }else {
+                map.put("指导教师(姓名)", "无");
+                map.put("指导教师(职称)", "无");
+            }
+            if(teacher2 != null){
+                if(teacher.getName() != null){
+                    map.put("企业导师(姓名)", teacher2.getName());
+                }else {
+                    map.put("企业导师(姓名)", "无");
+                }
+                if(teacher2.getTeacherTitle() != null){
+                    map.put("企业导师(职称)",teacher2.getTeacherTitle());
+                }else {
+                    map.put("企业导师(职称)","无");
+                }
+
+            }else {
+                map.put("企业导师(姓名)", "无");
+                map.put("企业导师(职称)","无");
+            }
+
+            mapArrayList.add(map);
+        }
+        System.out.println(mapArrayList);
+        ExcelUtils.getInstance().createExcel(mapArrayList,"data","2023届本科毕业设计（论文）题目汇总");
+
+        return  "下载成功";
+    }
+}
---
+++ src/main/java/com/sd/xingong/util/ExcelUtils.java
@@ -0,0 +1,202 @@
+package com.sd.xingong.util;
+
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.xssf.usermodel.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+/**
+ * @author river
+ * @date 2021/4/29
+ */
+public class ExcelUtils {
+
+    private static ExcelUtils instance = new ExcelUtils();
+
+    private ExcelUtils(){
+
+
+    }
+
+    public static ExcelUtils getInstance(){
+        return instance;
+    }
+
+    /**
+     * 将 List<Map<String,Object>> 类型的数据导出为 Excel
+     * 默认 Excel 文件的输出路径为 项目根目录下
+     * 文件名为 filename + 时间戳 + .xlsx
+     *
+     * @param mapList 数据源(通常为数据库查询数据)
+     * @param filename   文件名前缀, 实际文件名后会加上日期
+     * @param title   表格首行标题
+     * @return  文件输出路径
+     */
+    public String  createExcel(List<Map<String, Object>> mapList,String filename, String title) {
+        //获取数据源的 key, 用于获取列数及设置标题
+        Map<String, Object> map = mapList.get(0);
+
+        Set<String> stringSet = map.keySet();
+
+        ArrayList<String> headList = new ArrayList<>(stringSet);
+
+        System.out.println(headList);
+        //定义一个新的工作簿
+        XSSFWorkbook wb = new XSSFWorkbook();
+        //创建一个Sheet页
+        XSSFSheet sheet = wb.createSheet(title);
+        //设置行高
+        sheet.setDefaultRowHeight((short) (2 * 256));
+        //为有数据的每列设置列宽
+        for (int i = 0; i < headList.size(); i++) {
+            sheet.setColumnWidth(i, 8000);
+        }
+        //设置单元格字体样式
+        XSSFFont font = wb.createFont();
+        font.setFontName("等线");
+        font.setFontHeightInPoints((short) 16);
+
+        //在sheet里创建第一行，并设置单元格内容为 title (标题)
+        XSSFRow titleRow = sheet.createRow(0);
+        XSSFCell titleCell = titleRow.createCell(0);
+        titleCell.setCellValue(title);
+
+        //合并单元格CellRangeAddress构造参数依次表示起始行，截至行，起始列， 截至列
+        sheet.addMergedRegion(new CellRangeAddress(0, 0, 0, headList.size() - 1));
+        // 创建单元格文字居中样式并设置标题单元格居中
+        XSSFCellStyle cellStyle = wb.createCellStyle();
+        cellStyle.setAlignment(HorizontalAlignment.CENTER);
+        titleCell.setCellStyle(cellStyle);
+
+        //获得表格第二行
+        XSSFRow row = sheet.createRow(1);
+        //根据数据源信息给第二行每一列设置标题
+
+        int index[] = {1,10,9,6,7,8,5,12,3,0,11,2,4};
+
+
+        for (int i = 0; i < headList.size(); i++) {
+            XSSFCell cell = row.createCell(i);
+            cell.setCellValue(headList.get(index[i]));
+        }
+
+
+
+    /*    XSSFCell cell = row.createCell(0);
+        cell.setCellValue(headList.get(1));
+
+        cell = row.createCell(1);
+        cell.setCellValue(headList.get(10));
+
+        cell = row.createCell(2);
+        cell.setCellValue(headList.get(9));
+
+        cell = row.createCell(3);
+        cell.setCellValue(headList.get(6));
+
+        cell = row.createCell(4);
+        cell.setCellValue(headList.get(7));
+
+        cell = row.createCell(5);
+        cell.setCellValue(headList.get(8));
+
+        cell = row.createCell(6);
+        cell.setCellValue(headList.get(5));
+
+        cell = row.createCell(7);
+        cell.setCellValue(headList.get(12));
+
+        cell = row.createCell(8);
+        cell.setCellValue(headList.get(3));
+
+        cell = row.createCell(9);
+        cell.setCellValue(headList.get(0));
+
+        cell = row.createCell(10);
+        cell.setCellValue(headList.get(11));
+
+        cell = row.createCell(11);
+        cell.setCellValue(headList.get(2));
+
+        cell = row.createCell(12);
+        cell.setCellValue(headList.get(4));*/
+
+
+        XSSFRow rows;
+        XSSFCell cells;
+        //循环拿到的数据给所有行每一列设置对应的值
+
+        for (int i = 0; i < mapList.size(); i++) {
+            //在这个sheet页里创建一行
+            rows = sheet.createRow(i + 2);
+//            System.out.print("--");
+//            System.out.println(mapList.get(i).get("序号"));
+            //给该行数据赋值
+
+
+            for (int j = 0; j < headList.size(); j++) {
+               //  System.out.println(headList.size());
+               // System.out.println(mapList.get(i));
+
+                String value = mapList.get(i).get(headList.get(index[j])).toString();
+                cells = rows.createCell(j);
+                cells.setCellValue(value);
+            }
+
+        }
+
+        Date date = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
+        // 使用项目根目录, 文件名加上时间戳
+        String path = System.getProperty("user.dir") + "\\" + filename + dateFormat.format(date) + ".xlsx";
+        System.out.println("Excel文件输出路径: "+path);
+        try {
+            File file = new File(path);
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            wb.write(fileOutputStream);
+
+            fileOutputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return path;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("start...");
+        List<Map<String, Object>> mapArrayList = new ArrayList<>();
+       // int j =1;
+        for (int i = 0; i < 10; i++) {
+            Map<String, Object> map = new HashMap<>();
+
+            map.put("学生姓名", null);
+            map.put("学号", "学号");
+            map.put("专业", "专业");
+            map.put("班级", "班级");
+            map.put("设计（论文）题目", "设计（论文）题目");
+            map.put("成果类别(论文/设计)", "成果类别(论文/设计)");
+            map.put("选题性质(开题报告写法一致)", "选题性质(开题报告写法一致)");
+            map.put("选题来源(开题报告写法一致)", "选题来源(开题报告写法一致)");
+            map.put("序号", "序号");
+            map.put("指导教师(姓名)", "指导教师(姓名)");
+            map.put("指导教师(职称)", "指导教师(职称)");
+            map.put("企业导师(姓名)", "企业导师(姓名)");
+            map.put("企业导师(职称)", "企业导师(职称)");
+            mapArrayList.add(map);
+
+
+        }
+
+        System.out.println(mapArrayList.get(0));
+
+        System.out.println("end...");
+        ExcelUtils.getInstance().createExcel(mapArrayList,"data","2023届本科毕业设计（论文）题目汇总");
+
+    }
+
+}
